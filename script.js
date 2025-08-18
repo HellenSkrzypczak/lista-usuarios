5dc1677c1cf1577d8c706b48933252a3ecf8d46a
Hellen Skrzypczak <skr.hellen@gmail.com>
2025-08-18 16:31:43 -0300

    Ajustado validação dos inputs

--- script.js
+++ script.js
@@ -1,58 +1,68 @@
 const btnCarregarUsuarios = document.getElementById('btnCarregarUsuarios');
 const btnAdicionarUsuario = document.getElementById('btnAdicionarUsuario');
 const listaUsuarios = document.getElementById('listaUsuarios');
-const url = 'https://jsonplaceholder.typicode.com/users';
 
 let usuariosObj = []; // array global
+let usuariosCarregados = false;
 
-btnCarregarUsuarios.addEventListener('click', async () => {
-    const response = await fetch(url);
-    const usuariosObj = await response.json(); //transforma o dados da API em objeto
+async function buscarUsuarios(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    usuariosObj = await response.json(); //transforma o dados da API em objeto
     renderizarListaUsuarios(usuariosObj);
-});
+}
 
-btnAdicionarUsuario.addEventListener('click', () => {
-    adicionarNovoUsuario(usuariosObj);
-});
+function limparLista() { 
+    listaUsuarios.innerHTML = ""
+}
 
-
-function limparLista() { listaUsuarios.innerHTML = ''}
+function validacaoCampos(...campos) {
+    return campos.every(campos => campos.value.trim() !== "")
+}
 
 function adicionarNovoUsuario(usuariosObj){
-    const inputNome = document.getElementById('inputNome').value.trim();
-    const inputEmail = document.getElementById('inputEmail').value.trim();
-    const inputCidade = document.getElementById('inputCidade').value.trim();
+    const inputNome = document.getElementById('inputNome');
+    const inputEmail = document.getElementById('inputEmail');
+    const inputCidade = document.getElementById('inputCidade');
 
     const novoUsuarioOBJ = { 
-        name: inputNome, 
-        email: inputEmail, 
-        address:{ city: inputCidade}
+        name: inputNome.value.trim(), 
+        email: inputEmail.value.trim(), 
+        address:{ city: inputCidade.value.trim() }
     }
     
-    console.log(novoUsuarioOBJ);
-    alert("Usuario cadastrado com sucesso!");
-    usuariosObj.push(novoUsuarioOBJ); //adiciona o objeto ba array de usuarios
+    if(validacaoCampos(inputNome, inputEmail, inputCidade))
+    {
+        alert("Usuario cadastrado com sucesso!");
+        usuariosObj.push(novoUsuarioOBJ); //adiciona o objeto ba array de usuarios
+        renderizarListaUsuarios(usuariosObj);
+
+    }
+    else {
+        alert('Preencha todos os campos!');
+    }
 }
 
 function renderizarListaUsuarios(usuariosObj){
-    limparLista();
-    usuariosObj.map((usuario) => {
+    limparLista()
+    usuariosObj.forEach((usuario) => {
         const li = document.createElement("li");
         li.innerHTML = `Nome: ${usuario.name} - Email: ${usuario.email} - Cidade: ${usuario.address.city}`;
         li.classList.add('cssLista');
         listaUsuarios.appendChild(li);
     })
-    adicionarNovoUsuario();
 }
 
+btnCarregarUsuarios.addEventListener('click', async () => {
+    if(usuariosCarregados)
+    {
+        return;
+    }
+    else{
+        buscarUsuarios() 
+        usuariosCarregados = true;
+    }
+});
 
-
-
-    /*const usuariosComC = usuariosObj.filter(usuarios => usuarios.name.charAt(0) === 'C');
-    
-    usuariosComC.forEach((usuario) => {
-        const li = document.createElement("li")
-        li.innerText = `Nome: ${usuario.name} - Email: ${usuario.email} - Cidade: ${usuario.address.city}`;
-        listaUsuariosC.appendChild(li);
-    })*/
-
+btnAdicionarUsuario.addEventListener('click', () => {
+    adicionarNovoUsuario(usuariosObj);
+});
