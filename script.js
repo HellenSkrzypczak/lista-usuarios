b82adb3e8f560b6176a572c291926c6fbd676319
Hellen Skrzypczak <skr.hellen@gmail.com>
2025-08-19 16:52:21 -0300

    Adiocina button de excluir

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const listaUsuarios = document.getElementById('listaUsuarios');
 
 let usuariosObj = []; // array global
 let usuariosCarregados = false;
+let idUsuarioExcluido = null;
 
 async function buscarUsuarios(){
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -24,7 +25,11 @@ function adicionarNovoUsuario(usuariosObj){
     const inputEmail = document.getElementById('inputEmail');
     const inputCidade = document.getElementById('inputCidade');
 
+    let ids = usuariosObj.map(u => u.id);
+    let novoId = (ids.length > 0 ? Math.max(...ids) : 0) + 1;
+
     const novoUsuarioOBJ = { 
+        id: novoId, 
         name: inputNome.value.trim(), 
         email: inputEmail.value.trim(), 
         address:{ city: inputCidade.value.trim() }
@@ -42,14 +47,33 @@ function adicionarNovoUsuario(usuariosObj){
     }
 }
 
-function renderizarListaUsuarios(usuariosObj){
+function renderizarListaUsuarios(){
     limparLista()
     usuariosObj.forEach((usuario) => {
         const li = document.createElement("li");
         li.innerHTML = `Nome: ${usuario.name} - Email: ${usuario.email} - Cidade: ${usuario.address.city}`;
         li.classList.add('cssLista');
         listaUsuarios.appendChild(li);
+
+        const buttonExcluir = document.createElement("button");
+        buttonExcluir.innerText = "Excluir";
+        buttonExcluir.classList.add("cssButton");
+        li.appendChild(buttonExcluir);
+
+        buttonExcluir.addEventListener('click', () => {
+            idUsuarioExcluido = (usuario.id);
+            console.log(idUsuarioExcluido);
+            excluirUsuario();
+        });
     })
+}
+
+function excluirUsuario() {
+
+    let indice = usuariosObj.findIndex(usuario => usuario.id === idUsuarioExcluido)
+    const removidos = usuariosObj.splice(indice, 1);
+    //console.log(usuariosObj);
+    renderizarListaUsuarios();
 }
 
 btnCarregarUsuarios.addEventListener('click', async () => {
